eaf4f2bba18f6a021ded252e70af99779db7d1b6
florian oswald <florian.oswald@gmail.com>
2018-11-08 11:14:55 +0100

    cleanup

---
+++ docs/js/Rplot.js
@@ -0,0 +1,12 @@
+(function($) {
+    $(document).ready(function() {
+	
+	$('#Rplot').scianimator({
+	    'images': ['images/Rplot1.png', 'images/Rplot2.png', 'images/Rplot3.png', 'images/Rplot4.png', 'images/Rplot5.png', 'images/Rplot6.png', 'images/Rplot7.png', 'images/Rplot8.png', 'images/Rplot9.png', 'images/Rplot10.png', 'images/Rplot11.png', 'images/Rplot12.png', 'images/Rplot13.png', 'images/Rplot14.png', 'images/Rplot15.png', 'images/Rplot16.png', 'images/Rplot17.png', 'images/Rplot18.png', 'images/Rplot19.png', 'images/Rplot20.png'],
+	    'width': 1000,
+	    'delay': 500,
+	    'loopMode': 'loop'
+	});
+	$('#Rplot').scianimator('play');
+    });
+})(jQuery);
